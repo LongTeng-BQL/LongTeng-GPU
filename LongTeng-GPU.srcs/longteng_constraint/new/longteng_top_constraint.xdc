ddc465d03f6b5e8eb4c6023ad69c3246fd3c5220
Igniculus Fu <1041154549@qq.com>
2024-10-20 21:23:56 +0800

    Update: introduced HDMI stuff.

--- LongTeng-GPU.srcs/longteng_constraint/new/longteng_top_constraint.xdc
+++ LongTeng-GPU.srcs/longteng_constraint/new/longteng_top_constraint.xdc
@@ -12,6 +12,7 @@ set_property PACKAGE_PIN A24 [get_ports pcie_ready]
 set_property IOSTANDARD LVCMOS33 [get_ports pcie_ready]
 
 # Clk.
+create_clock -period 20.000 -name clk_50m [get_ports clk_50m]
 set_property PACKAGE_PIN G22 [get_ports clk_50m]
 set_property IOSTANDARD LVCMOS33 [get_ports clk_50m]
 set_property PACKAGE_PIN K6 [get_ports {clk_pcie_ref_clk_p[0]}]
@@ -21,10 +22,24 @@ set_property PACKAGE_PIN A12 [get_ports pcie_reset]
 set_property IOSTANDARD LVCMOS33 [get_ports pcie_reset]
 
 # PCIe.
+set_property LOC GTXE2_CHANNEL_X0Y0 [get_cells {longteng_top_i/pcie_controller/inst/comp_axi_enhanced_pcie/comp_enhanced_core_top_wrap/axi_pcie_enhanced_core_top_i/pcie_7x_v2_0_2_inst/pcie_top_with_gt_top.gt_ges.gt_top_i/pipe_wrapper_i/pipe_lane[3].gt_wrapper_i/gtx_channel.gtxe2_channel_i}]
 set_property PACKAGE_PIN R4 [get_ports {pcie_connector_rxp[3]}]
+set_property LOC GTXE2_CHANNEL_X0Y1 [get_cells {longteng_top_i/pcie_controller/inst/comp_axi_enhanced_pcie/comp_enhanced_core_top_wrap/axi_pcie_enhanced_core_top_i/pcie_7x_v2_0_2_inst/pcie_top_with_gt_top.gt_ges.gt_top_i/pipe_wrapper_i/pipe_lane[2].gt_wrapper_i/gtx_channel.gtxe2_channel_i}]
 set_property PACKAGE_PIN N4 [get_ports {pcie_connector_rxp[2]}]
+set_property LOC GTXE2_CHANNEL_X0Y2 [get_cells {longteng_top_i/pcie_controller/inst/comp_axi_enhanced_pcie/comp_enhanced_core_top_wrap/axi_pcie_enhanced_core_top_i/pcie_7x_v2_0_2_inst/pcie_top_with_gt_top.gt_ges.gt_top_i/pipe_wrapper_i/pipe_lane[1].gt_wrapper_i/gtx_channel.gtxe2_channel_i}]
 set_property PACKAGE_PIN L4 [get_ports {pcie_connector_rxp[1]}]
+set_property LOC GTXE2_CHANNEL_X0Y3 [get_cells {longteng_top_i/pcie_controller/inst/comp_axi_enhanced_pcie/comp_enhanced_core_top_wrap/axi_pcie_enhanced_core_top_i/pcie_7x_v2_0_2_inst/pcie_top_with_gt_top.gt_ges.gt_top_i/pipe_wrapper_i/pipe_lane[0].gt_wrapper_i/gtx_channel.gtxe2_channel_i}]
 set_property PACKAGE_PIN J4 [get_ports {pcie_connector_rxp[0]}]
+
+#HDMI.
+set_output_delay -clock clk_50m 0.100 [get_ports HDMI_TMDS_clk_p]
+set_output_delay -clock clk_50m 0.100 [get_ports HDMI_TMDS_clk_n]
+set_property PACKAGE_PIN F17 [get_ports HDMI_TMDS_clk_p]
+set_property PACKAGE_PIN J15 [get_ports {HDMI_TMDS_data_p[0]}]
+set_property PACKAGE_PIN E15 [get_ports {HDMI_TMDS_data_p[1]}]
+set_property PACKAGE_PIN G17 [get_ports {HDMI_TMDS_data_p[2]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {hdmi_enable[0]}]
+set_property PACKAGE_PIN E22 [get_ports {hdmi_enable[0]}]
 
 # Common Configurations.
 set_property BITSTREAM.CONFIG.SPI_BUSWIDTH 4 [current_design]
@@ -34,3 +49,9 @@ set_property BITSTREAM.GENERAL.COMPRESS TRUE [current_design]
 set_property BITSTREAM.CONFIG.UNUSEDPIN Pullup [current_design]
 set_property CFGBVS VCCO [current_design]
 set_property CONFIG_VOLTAGE 3.3 [current_design]
+
+create_clock -period 10.000 -name {clk_pcie_ref_clk_p[0]} -waveform {0.000 5.000} [get_ports {clk_pcie_ref_clk_p[0]}]
+set_clock_groups -physically_exclusive -group [get_clocks -include_generated_clocks oobclk_125mhz_master_lane0_X0Y0] -group [get_clocks -include_generated_clocks oobclk_250mhz_master_lane0_X0Y0]
+set_clock_groups -physically_exclusive -group [get_clocks -include_generated_clocks oobclk_125mhz_master_lane1_X0Y0] -group [get_clocks -include_generated_clocks oobclk_250mhz_master_lane1_X0Y0]
+set_clock_groups -physically_exclusive -group [get_clocks -include_generated_clocks oobclk_125mhz_master_lane2_X0Y0] -group [get_clocks -include_generated_clocks oobclk_250mhz_master_lane2_X0Y0]
+set_clock_groups -physically_exclusive -group [get_clocks -include_generated_clocks oobclk_125mhz_master_lane3_X0Y0] -group [get_clocks -include_generated_clocks oobclk_250mhz_master_lane3_X0Y0]
--- LongTeng-GPU.srcs/longteng_sources/imports/hdl/longteng_top_wrapper.v
+++ LongTeng-GPU.srcs/longteng_sources/imports/hdl/longteng_top_wrapper.v
@@ -2,7 +2,7 @@
 //Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2023.2 (lin64) Build 4029153 Fri Oct 13 20:13:54 MDT 2023
-//Date        : Sat Sep 28 18:42:23 2024
+//Date        : Sun Oct 20 20:07:18 2024
 //Host        : fhh-pc-linux running 64-bit Arch Linux
 //Command     : generate_target longteng_top_wrapper.bd
 //Design      : longteng_top_wrapper
@@ -26,16 +26,23 @@ module longteng_top_wrapper
     DDR3_0_ras_n,
     DDR3_0_reset_n,
     DDR3_0_we_n,
+    HDMI_DDC_scl_io,
+    HDMI_DDC_sda_io,
+    HDMI_TMDS_clk_n,
+    HDMI_TMDS_clk_p,
+    HDMI_TMDS_data_n,
+    HDMI_TMDS_data_p,
     clk_50m,
     clk_pcie_ref_clk_n,
     clk_pcie_ref_clk_p,
     ddr3_ready,
-    pcie_reset,
+    hdmi_enable,
     pcie_connector_rxn,
     pcie_connector_rxp,
     pcie_connector_txn,
     pcie_connector_txp,
-    pcie_ready);
+    pcie_ready,
+    pcie_reset);
   output [14:0]DDR3_0_addr;
   output [2:0]DDR3_0_ba;
   output DDR3_0_cas_n;
@@ -51,16 +58,23 @@ module longteng_top_wrapper
   output DDR3_0_ras_n;
   output DDR3_0_reset_n;
   output DDR3_0_we_n;
+  inout HDMI_DDC_scl_io;
+  inout HDMI_DDC_sda_io;
+  output HDMI_TMDS_clk_n;
+  output HDMI_TMDS_clk_p;
+  output [2:0]HDMI_TMDS_data_n;
+  output [2:0]HDMI_TMDS_data_p;
   input clk_50m;
   input [0:0]clk_pcie_ref_clk_n;
   input [0:0]clk_pcie_ref_clk_p;
   output ddr3_ready;
-  input pcie_reset;
+  output [0:0]hdmi_enable;
   input [3:0]pcie_connector_rxn;
   input [3:0]pcie_connector_rxp;
   output [3:0]pcie_connector_txn;
   output [3:0]pcie_connector_txp;
   output pcie_ready;
+  input pcie_reset;
 
   wire [14:0]DDR3_0_addr;
   wire [2:0]DDR3_0_ba;
@@ -77,17 +91,40 @@ module longteng_top_wrapper
   wire DDR3_0_ras_n;
   wire DDR3_0_reset_n;
   wire DDR3_0_we_n;
+  wire HDMI_DDC_scl_i;
+  wire HDMI_DDC_scl_io;
+  wire HDMI_DDC_scl_o;
+  wire HDMI_DDC_scl_t;
+  wire HDMI_DDC_sda_i;
+  wire HDMI_DDC_sda_io;
+  wire HDMI_DDC_sda_o;
+  wire HDMI_DDC_sda_t;
+  wire HDMI_TMDS_clk_n;
+  wire HDMI_TMDS_clk_p;
+  wire [2:0]HDMI_TMDS_data_n;
+  wire [2:0]HDMI_TMDS_data_p;
   wire clk_50m;
   wire [0:0]clk_pcie_ref_clk_n;
   wire [0:0]clk_pcie_ref_clk_p;
   wire ddr3_ready;
-  wire ext_reset_in_0;
+  wire [0:0]hdmi_enable;
   wire [3:0]pcie_connector_rxn;
   wire [3:0]pcie_connector_rxp;
   wire [3:0]pcie_connector_txn;
   wire [3:0]pcie_connector_txp;
   wire pcie_ready;
+  wire pcie_reset;
 
+  IOBUF HDMI_DDC_scl_iobuf
+       (.I(HDMI_DDC_scl_o),
+        .IO(HDMI_DDC_scl_io),
+        .O(HDMI_DDC_scl_i),
+        .T(HDMI_DDC_scl_t));
+  IOBUF HDMI_DDC_sda_iobuf
+       (.I(HDMI_DDC_sda_o),
+        .IO(HDMI_DDC_sda_io),
+        .O(HDMI_DDC_sda_i),
+        .T(HDMI_DDC_sda_t));
   longteng_top longteng_top_i
        (.DDR3_0_addr(DDR3_0_addr),
         .DDR3_0_ba(DDR3_0_ba),
@@ -104,14 +141,25 @@ module longteng_top_wrapper
         .DDR3_0_ras_n(DDR3_0_ras_n),
         .DDR3_0_reset_n(DDR3_0_reset_n),
         .DDR3_0_we_n(DDR3_0_we_n),
+        .HDMI_DDC_scl_i(HDMI_DDC_scl_i),
+        .HDMI_DDC_scl_o(HDMI_DDC_scl_o),
+        .HDMI_DDC_scl_t(HDMI_DDC_scl_t),
+        .HDMI_DDC_sda_i(HDMI_DDC_sda_i),
+        .HDMI_DDC_sda_o(HDMI_DDC_sda_o),
+        .HDMI_DDC_sda_t(HDMI_DDC_sda_t),
+        .HDMI_TMDS_clk_n(HDMI_TMDS_clk_n),
+        .HDMI_TMDS_clk_p(HDMI_TMDS_clk_p),
+        .HDMI_TMDS_data_n(HDMI_TMDS_data_n),
+        .HDMI_TMDS_data_p(HDMI_TMDS_data_p),
         .clk_50m(clk_50m),
         .clk_pcie_ref_clk_n(clk_pcie_ref_clk_n),
         .clk_pcie_ref_clk_p(clk_pcie_ref_clk_p),
         .ddr3_ready(ddr3_ready),
-        .pcie_reset(pcie_reset),
+        .hdmi_enable(hdmi_enable),
         .pcie_connector_rxn(pcie_connector_rxn),
         .pcie_connector_rxp(pcie_connector_rxp),
         .pcie_connector_txn(pcie_connector_txn),
         .pcie_connector_txp(pcie_connector_txp),
-        .pcie_ready(pcie_ready));
+        .pcie_ready(pcie_ready),
+        .pcie_reset(pcie_reset));
 endmodule
